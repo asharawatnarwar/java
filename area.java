4abc25ae8d438bf70c0790b49f141f3aabe98f46
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-03 11:21:12 +0530

    area

---
+++ area.java
@@ -0,0 +1,13 @@
+ class area {
+    {
+        public static void main(String arr[])
+       {
+        int lenght ,breath,area;
+         lenght=10;
+         breath=5;
+         area=lenght*breath;
+        
+        System.out.println("Area of Raectangle " +area);
+       } 
+    
+}
