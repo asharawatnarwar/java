a44e24bd77a46832440dcb6ef1b8d7d56af9641c
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-03 11:19:07 +0530

    userinput

---
+++ Usreinput.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+class Add{
+    public static void main(String arr[])
+    {
+        int a;
+        Scanner ob = new Scanner(System.in);
+         System.out.print("Enter a no.");
+         a = ob.nextInt();
+          
+         System.out.println("You entered "+a);
+
+    }
+    
+}
