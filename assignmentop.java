3b6b3458562a525470cec5f7af3af7c70eccc225
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-06 21:57:09 +0530

    Assignmentopretior

---
+++ assignmentop.java
@@ -0,0 +1,12 @@
+class Demo
+{
+    public static void main (String arr[])
+    {
+        int a=10;
+
+        a +=5;
+        System.out.println("a =" +a);
+        a -=5;
+        System.out.println("a =" +a);
+    }
+}
