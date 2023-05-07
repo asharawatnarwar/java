db0ff88e85f05ecc7748082b48521c642a134604
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-07 20:44:58 +0530

    relationopretor

---
+++ relationalop.java
@@ -0,0 +1,14 @@
+class Demo
+{
+  public static void main(String arr[])
+  {
+
+    System.out.println(5<3);
+    System.out.println(5>3);
+    System.out.println(5>=5);
+    System.out.println(3<=5);
+    System.out.println(5==5);
+    System.out.println(5!=5);
+
+  }
+}
