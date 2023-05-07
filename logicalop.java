79645e12f3b7a0917e07544c0536e5e21bf05a7c
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-07 20:44:33 +0530

    logicalopretor

---
+++ logicalop.java
@@ -0,0 +1,17 @@
+class Demo
+{
+  public static void main(String arr[])
+  {
+
+ System.out.println(5<3 && 6>8);
+ System.out.println(5<3 && 6<8);
+ System.out.println(5>3 && 6>8);
+ System.out.println(5>3 && 6<8);
+
+ System.out.println(5<3 || 6>8);
+ System.out.println(5<3 || 6<8);
+ System.out.println(5>3 || 6>8);
+ System.out.println(5>3 || 6<8);
+ 
+  }
+}
