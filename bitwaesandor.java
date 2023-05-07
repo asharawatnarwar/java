4d6debd316223b153690c1a4e6ed67e064ef3c1c
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-07 20:43:26 +0530

    bitwaesopretor

---
+++ bitwaesandor.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Demo{
+    public static void main(String arr[])
+    {
+        int a;
+     a = 6 & 5;
+     System.out.println("a = " + a);
+      a = 6 | 5;
+     System.out.println("a = " + a);
+      a = 6 ^ 5;
+     System.out.println("a = " + a);
+
+    }
+} 
