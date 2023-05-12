c9029f7c6bd80685c9f7933544f23cfd4d837da7
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-12 11:54:17 +0530

    boolean

---
+++ boolean.java
@@ -0,0 +1,11 @@
+class Demo
+{
+    public static void main(String arr[])
+    {
+      boolean b= false;
+      if(b = true)
+      System.out.println("ABC");
+      else 
+    System.out.println("XYZ");
+    }
+}
