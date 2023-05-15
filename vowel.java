24517e5cbb7216450837d66cf145016f2c180663
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-15 10:27:36 +0530

    vowel

--- vowel.java
+++ vowel.java
@@ -9,7 +9,7 @@ class Demo
     System.out.print("Enter a no.:");
     a = ob.next();
 
-    if ( a == "a" || a=="u" || a=="i" || a=="o" || a=="e" )
+    if ( a == "a" || a=="u" || a=="i" || a=="o" || a=="e"|| a == "A" || a=="U" || a=="I" || a=="O" || a=="E" )
          {
             System.out.println(" you enter Positive no.");
          }
