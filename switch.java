aa1def20325286076d10d7a204f236b8d688c992
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-15 09:57:47 +0530

    Switch

---
+++ switch.java
@@ -0,0 +1,25 @@
+class Demo
+{
+    public static void main (String arr [])
+    {
+        int a=2;
+
+        switch (a) {
+            case 1:
+                System.out.println("One");
+                break;
+              case 2:
+              System.out.println("Two");
+              break;
+              case 3:
+              System.out.println("Three");
+              break;
+              case 4:
+              System.out.println("Four");
+              break;
+            default:
+                break;
+        }
+        System.out.println("Bye.....");
+    }
+} 
