869df1d7c4c28337f457ee9d928c03c6872c05a4
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-15 09:58:00 +0530

    String

---
+++ stringrevres.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class rev
+{
+    public static void main(String arr[])
+   
+    {
+        String st;
+        int len,i;
+        Scanner ob=new Scanner(System.in);
+        System.out.print("enter a string");
+       st = ob.nextLine();
+        len=st.length();
+         for (i = len-1; i>=0;i--)
+{
+    System.out.println(st.charAt(i));
+}
+
+    }
+}
