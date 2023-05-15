70fc8c7a6685a2468ff0b64d7e77035c79b2b3ac
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-15 09:48:52 +0530

    countthree

---
+++ countthree.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Demo
+{
+    public static void main (String arr[])
+    {
+        int num, i,sum = 0;
+        Scanner ob =new Scanner(System.in);
+        for(i=1;i<=10;i++)
+        {
+        System.out.print("Enter the no.");
+        num= ob.nextInt();
+        if( i % 3 == 0)
+        {
+
+        sum += num;
+        System.out.print("sum= "+sum); 
+        }
+    }
+   }
+}
+
