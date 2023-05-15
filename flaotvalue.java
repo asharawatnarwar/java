86c0e0b938395cc94be53e94158c2796768528ec
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-15 09:48:29 +0530

    flaotvalue

---
+++ flaotvalue.java
@@ -0,0 +1,12 @@
+class Demo
+{
+    public static void main(String arr[])
+    {
+      float  a=4f;
+      if(a == 4.0)
+      System.out.println("Equal");
+      else 
+    System.out.println("Not Equal");
+    System.out.println("hi");
+    }
+}
