9b53f9923aa7a1c0abc25d87dfad986b0f1172c0
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-16 21:56:20 +0530

    Starpattern

---
+++ star.java
@@ -0,0 +1,18 @@
+class Star
+{
+    public static void main(String arr[])
+    {
+        int i,j;
+        for (i=1;i<=5;i++)
+        {
+            {
+                System.out.print(" ");
+            }
+            for (j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
