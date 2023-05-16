b6f6a02d6522652219b5cae610ada91d4a9e00fd
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-16 21:55:10 +0530

    oddnumberin for loop

---
+++ hundreadodd.java
@@ -0,0 +1,14 @@
+class odd
+{
+    public static void main(String arr[])
+    {
+        int i ;
+        for (i=0;i<=100;i++)
+        {
+            if (!(i % 2==0 ))
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
