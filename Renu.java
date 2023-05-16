48a1de556b4fd675aeae7dc85a0481589111b511
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-16 21:55:45 +0530

    demo

--- Renu.java
+++ Renu.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 class Renu
 {
-    public static void main(string arr[])
+    public static void main(String arr[])
     {
         int i,n,fact=1;
-        Scanner ob.new Scanner(String arr[])
+        Scanner ob=new Scanner(System.in);
         System.out.println("enter a no");
         n=ob.nextInt();
         for(i=0;i<n;i++)
         fact=fact*i;
         {
-            System,out.println("fact"+fact);
+            System.out.println("fact"+fact);
         }
     }
 }
