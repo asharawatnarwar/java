3ae66d0b3d5f42554097e2889245e114c05f9af5
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-16 21:56:35 +0530

    pattern

---
+++ starpattern.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Star
+{
+    public static void main(String arr[])
+    {
+        int i,j;
+        for (i=1;i<=5;i++)
+        {
+            for (j=3;j<=4;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+//import java.util.Scanner;
+class Demo
+{
+    public static void main(String arr[])
+    {
+        int i,j;
+        for (i=1;i<=5;i++)
+        {
+            for (j=3;j<=5;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
