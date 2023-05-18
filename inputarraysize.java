37d35dac71b64f3d29011371874267963e330766
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-18 11:55:10 +0530

    inputarraysize

---
+++ inputarraysize.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Array
+{
+    public static void main(String arr[])
+    {
+        int a[];
+         int size;
+         a = new int [size];
+            Scanner ob = new Scanner(System.in);
+         for (int i=0;i<a.length;i++)
+
+         System.out.print("Enter the size of array :");
+         a[size] = ob.nextInt();
+         {
+            System.out.print(" " +a[i]);
+         }
+    }
+}
