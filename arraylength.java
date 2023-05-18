dda8d6b82eb9472347e70287ee88a08de242d35d
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-18 11:53:23 +0530

    arraylenght

---
+++ arraylength.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Length
+{
+    public static void main(String arr[])
+    {
+        int a[] ={11,45,32,34,56,54};
+        int len = a.length;
+
+        System.out.println(len);
+        
+        for (int i=0;i<len ;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
