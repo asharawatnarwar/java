952fd2fcf42e928b7b1b817882b9fd37d954eace
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-18 11:53:47 +0530

    arraysizeinjava

---
+++ arraysize.java
@@ -0,0 +1,14 @@
+class My 
+{
+    public static void main(String arr[])
+    {
+        int a[];
+         int size = 5 ;
+         a = new int [size];
+
+         for (int i=0;i<a.length;i++)
+         {
+            System.out.print(" " +a[i]);
+         }
+    }
+}
