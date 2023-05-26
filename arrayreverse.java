1ec16930b5233c3d46fa272a584222e59e4ed8a8
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-26 21:33:20 +0530

    arrayreverse

---
+++ arrayreverse.java
@@ -0,0 +1,16 @@
+import java.time.Year;
+
+public class arrayreverse {
+          public static void main(String arr[])
+          {
+            int a[] = {1,3,4,5,54};
+            int b[];
+             b = fun(x);
+             for(int i:b)
+             {
+                System.out.println(i);
+             }
+             static int[] fun(int x )
+             
+          }
+}
