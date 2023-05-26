ef84f85bdaf17bcafe204a8a5a2a0e5645d5555c
asharawatnarwar <asharawatnarwar@gmail.com>
2023-05-26 21:43:40 +0530

    passing1darray

---
+++ passing1darray.java
@@ -0,0 +1,13 @@
+class Functionarray{
+    public static void main (String arr[])
+    {
+        int []x={2,3,4,5,6};
+        fun(x);
+
+    }
+    static void fun (int a[])
+    {
+      for(int i =a)
+      System.out.println(i);   
+    }
+}
