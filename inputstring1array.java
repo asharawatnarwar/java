6724db4f670790af44925f7023570418134ded1d
asharawatnarwar <asharawatnarwar@gmail.com>
2023-06-06 11:08:11 +0530

    inputstring

---
+++ inputstring1array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class inputstring1array {
+    public static void main(String arr[])
+    {
+        String st [] = new String [5];
+        Scanner ob = new Scanner(System.in);
+        for(int i=0;i<4;i++)
+        {
+            System.out.print("Enter the string :");
+            st[i]=ob.next();
+        }
+        fun(st);
+    }
+    static String[] fun(String a[])
+    {
+        for(int i=4;i<=0;i--)
+        return a;
+    }
+    
+}
